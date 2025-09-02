a9786580eb4072bbfc2af36def8e31d54801f982
Malavika S <malavikas343@gmail.com>
2025-09-02 17:33:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
 # Homepage (currently just a test)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 # Contact form submission
+
+
 @app.route('/contact', methods=['POST'])
 def contact():
     name = request.form.get('name')
@@ -18,6 +27,7 @@ def contact():
     print(f"📩 New Contact - Name: {name}, Email: {email}, Message: {message}")
 
     return jsonify({"status": "success", "message": "Your message has been received!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
