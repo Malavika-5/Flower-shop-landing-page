10c786cd86b1bf21dd0073a6e08891992f1cce2e
Malavika S <malavikas343@gmail.com>
2025-11-28 21:45:07 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from app import app as application
+import sys
+import os
+
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.append(project_home)
